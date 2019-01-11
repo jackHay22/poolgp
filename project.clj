039659c593485d53016376b27f64b665f63d3c85
jackHay22 <jjphay@gmail.com>
2019-01-10 20:29:07 -0500

    change project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject poolgp "0.1.0-SNAPSHOT"
   :description "Pool genetic programming simulation"
-  :url "https://poolgp.jackhay.io"
+  :url "https://github.com/jackHay22/poolgp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
