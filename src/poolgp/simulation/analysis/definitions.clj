18e82cdea77ca3003ff9b34056bf2a161aa0ea32
jackHay22 <jjphay@gmail.com>
2019-01-24 20:39:36 -0500

    updates to complex analytics functions

--- src/poolgp/simulation/analysis/definitions.clj
+++ src/poolgp/simulation/analysis/definitions.clj
@@ -8,6 +8,16 @@
   (if (= (:id (:current gs)) id)
       (:current gs) (:waiting gs)))
 
+(defn- player-is-current?
+  "check if player is current player"
+  [gs id]
+  (= id (:id (:current gs))))
+
+(defn- on-current-unrecorded
+  [gs current-val id]
+  ;TODO: need to simplify analytic internal code
+  )
+
 (defprotocol Analyze
   (update-analytic [a s])
   (display [a]))
@@ -25,17 +35,31 @@
 
 ; ----------- ANALYTICS DEFINITIONS -----------
 (defn score_ [id]
-  (Analytic. "score" #(max %1 (:score (get-player %2 id)))
-                    0 (fn [a] (:value a))))
+  (Analytic. (str "score/" id)
+             #(max %1 (:score (get-player %2 id)))
+             0 (fn [a] (:value a))))
 
 (defn scored_turns_ [id]
-  (Analytic. (str "scored_turns" id)
-    (fn [current-val a-state]
-      ;TODO
-      ) {:total 0} (fn [a] (:total (:value a)))))
+  (Analytic. (str "scored_turns/" id)
+    (fn [current-val g-state]
+      (if (and (player-is-current? g-state id)
+               (not (:recorded-current-turn current-val)))
+          (assoc
+            (if (:current-scored? g-state)
+                (update current-val :total inc)
+                current-val)
+            :recorded-current-turn true)
+          (if (and (:ready g-state)
+                   (:recorded-current-turn current-val))
+              (update current-val :recorded-current-turn not)
+              current-val)))
+    {:total 0 :recorded-current-turn false}
+    (fn [a] (:total (:value a)))))
 
 (defn scratches_ [id]
-  (Analytic. (str "scratches" id)
-    (fn [current-val a-state]
-      ;TODO
-      ) {:total 0} (fn [a] (:total (:value a)))))
+  (Analytic. (str "scratches/" id)
+    (fn [current-val g-state]
+      current-val ;TODO
+      )
+    {:total 0 :recorded-current-turn false}
+    (fn [a] (:total (:value a)))))
