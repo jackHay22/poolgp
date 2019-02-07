b762e64f181d6fc4bc9b0c1d79cf5513d96e0cc2
jackHay22 <jjphay@gmail.com>
2019-02-07 15:34:01 -0500

    fix

--- src/poolgp/simulation/players/push.clj
+++ src/poolgp/simulation/players/push.clj
@@ -21,11 +21,15 @@
         vec-float-stack (:vector_float push-final-state)
         int-stack (:integer push-final-state)]
         (cond
-          (not (empty? vec-float-stack)) (Vector. (first (first vec-float-stack))
-                                                  (second (first vec-float-stack)))
-          (not (empty? vec-int-stack)) (Vector. (first (first vec-int-stack))
-                                                (second (first vec-int-stack)))
-          (not (empty? int-stack))
+          (not (empty? vec-float-stack))
+                  (Vector. (first (first vec-float-stack))
+                           (second (first vec-float-stack)))
+          (not (empty? vec-int-stack))
+                  (Vector. (first (first vec-int-stack))
+                           (second (first vec-int-stack)))
+          (>= (count int-stack) 2)
+                  (Vector. (first int-stack)
+                           (second int-stack))
           :no-output (Vector. 0 0))))
 
 (defn eval-push
