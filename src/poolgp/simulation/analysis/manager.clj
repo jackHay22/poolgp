938c8dc01fadd9f088cbfe46f18101768cf2bc59
jackHay22 <jjphay@gmail.com>
2019-02-10 22:54:03 -0500

    reorganize analytics code

--- src/poolgp/simulation/analysis/manager.clj
+++ src/poolgp/simulation/analysis/manager.clj
@@ -1,8 +1,14 @@
 (ns poolgp.simulation.analysis.manager
   (:require [poolgp.simulation.analysis.game.manager :as game-manager]
-            [poolgp.simulation.analysis.analytics :as analytics])
+            [poolgp.simulation.analysis.definitions :as analytics-defs])
   (:import poolgp.simulation.structs.AnalysisState)
+  (:import poolgp.simulation.structs.PlayerAnalytics)
   (:gen-class))
+
+(def ANALYTICS (list analytics-defs/score
+                     analytics-defs/forward-movement
+                     analytics-defs/scratches
+                     analytics-defs/scored-turns))
 
 (defn analysis-init
   "initialize gamestate and analytics"
@@ -12,17 +18,32 @@
       #(AnalysisState.
         ;game state
         (game-manager/game-init (:game %) images?)
-        (analytics/state)
-        (analytics/state))
+        (PlayerAnalytics.
+          0 {:avg 0 :prev 0}
+          0 {:count 0 :best 0})
+        (PlayerAnalytics.
+          0 {:avg 0 :prev 0}
+          0 {:count 0 :best 0}))
     analysis-vec-json)))
 
 (defn analysis-update
-  "take analytics state, update gamestate and
-  perform analytics measurements"
+  "update analytics based on current gamestate"
   [analysis-state]
-  (analytics/update-analytics
-    (update-in analysis-state
-      [:game-state] game-manager/game-update)))
+  (let [game-updated-state (update-in analysis-state
+                              [:game-state] game-manager/game-update)]
+    (if (:ready? (:game-state game-updated-state))
+      (let [update-fn #(reduce (fn [state transform]
+                                  (transform
+                                      (:game-state game-updated-state) state %2))
+                              %1 ANALYTICS)]
+                              (println "P1" (:p1-analytics game-updated-state))
+                              (println "P2" (:p2-analytics game-updated-state))
+          (update-in
+            (update-in
+              game-updated-state
+              [:p1-analytics] update-fn :p1)
+              [:p2-analytics] update-fn :p2))
+      game-updated-state)))
 
 (defn analysis-render
   "display gamestate and display analytics"
@@ -32,5 +53,4 @@
 
 (defn analysis-log
   "write analysis logs"
-  [analysis-state]
-  (analytics/report-analytics analysis-state))
+  [analysis-state])
