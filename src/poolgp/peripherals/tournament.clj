9d3133d56b064a47fc4aa0b4eaab603b5615e74e
jackHay22 <jjphay@gmail.com>
2019-04-23 16:52:36 -0400

    add results reporting

--- src/poolgp/peripherals/tournament.clj
+++ src/poolgp/peripherals/tournament.clj
@@ -9,8 +9,21 @@
   "takes final simulation state and
   returns 1 or 0 or 0.5 for tie"
   [final-state]
+  ;TODO
   1
   )
+
+(defn- write-tournament-report
+  "takes final results set"
+  [results]
+  (do
+    (log/write-info "Best 3 individuals from tournament:")
+    (doall
+      (map #(log/write-info
+              (str (+ %2 1) ". ID: " (:id %1) ", WINS: " (:wins %1)))
+        (take 3
+          (sort-by :wins results))
+        (range)))))
 
 (defn run-tournament
   "run a tournament for a configured set of individuals"
@@ -19,26 +32,25 @@
         max-cycles (:max-iterations simulation-state)]
     (if (:tournament json-config)
       ;run complete tournament
-      (println
-      (let [indiv-pool (:entrants (utils/read-json-file (:tournament json-config)))]
-          (map (fn [i1]
-              (assoc i1 :wins
-                ;sum game results for individual
-                (reduce +
-                  (map (fn [i2]
-                        ;get pts from final state
-                        (calc-pts
-                          (loop [current 0
-                                 state (assoc simulation-state
-                                            :p1 (player-manager/init-clojush-player i1 :p1)
-                                            :p2 (player-manager/init-clojush-player i2 :p2))]
-                                 (if (> max-cycles current)
-                                   ;TODO: calculate fitness
-                                   (recur (inc current)
-                                          (doall (simulation/simulation-update state)))
-                                 state))))
-                       indiv-pool))))
-              indiv-pool))
-              ) ;end print
+      (write-tournament-report
+        (let [indiv-pool (:entrants (utils/read-json-file (:tournament json-config)))]
+            (map (fn [i1]
+                (assoc i1 :wins
+                  ;sum game results for individual
+                  (reduce +
+                    (map (fn [i2]
+                          ;get pts from final state
+                          (calc-pts
+                            (loop [current 0
+                                   state (assoc simulation-state
+                                              :p1 (player-manager/init-clojush-player i1 :p1)
+                                              :p2 (player-manager/init-clojush-player i2 :p2))]
+                                   (if (> max-cycles current)
+                                     ;TODO: calculate fitness
+                                     (recur (inc current)
+                                            (doall (simulation/simulation-update state)))
+                                   state))))
+                         indiv-pool))))
+                indiv-pool)))
       (log/write-error
         "The task definition must include a tournament file to use this mode"))))
